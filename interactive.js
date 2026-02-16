86d7f5bb80eafbeabbddcd7cff75ed9f60276f10
mxdalena <madalena.ovar@gmail.com>
2026-02-16 14:32:46 +0000

    Update interactive.js

--- interactive.js
+++ interactive.js
@@ -16,7 +16,8 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "";
+      if (i=0) {slides[1].style.display = "block"};
+      else {slides[i].style.display = ""};
     }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
